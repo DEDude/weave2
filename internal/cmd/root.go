c901fbd97ab09674f5d70c8fee6bac44a0f0f34d
DeDude <iwandyke@hotmail.co.uk>
2025-12-21 02:11:45 +0000

    added planning documentatation and standard structure

---
+++ internal/cmd/root.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "weave2",
+	Short: "Weave notes to RDF/graph",
+	Run: func(cmd *cobra.Command, args []string) {
+		//TODO: add subcommands
+	},
+}
